ae0666c7414891e9758e32331817f6a4b40de233
Mahum-Haider <mahum.haider@gmail.com>
2022-03-24 10:40:50 -0400

    fixed URL

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://fiimlvllfafaeo:47eb72e9881ff398082c0281e2a0e9e4319c6e595c8b5b0e5ee023f975cc5eb2@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d4hks3514jq9sj"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
