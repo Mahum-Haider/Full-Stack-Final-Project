5752a6aee1a59f0652d4b0b0909f8c24d8edac7f
Mahum-Haider <still fixing>
2022-03-24 15:54:22 -0400

    fixing

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgres://fiimlvllfafaeo:47eb72e9881ff398082c0281e2a0e9e4319c6e595c8b5b0e5ee023f975cc5eb2@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d4hks3514jq9sj"
+export DATABASE_URL="postgres://lxemfbahbfxzde:b9504db7cbe4a72c268ce39574110196810e6455597892244fa07257a974ad65@ec2-52-201-124-168.compute-1.amazonaws.com:5432/d8qrgg0tr3l56l"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
